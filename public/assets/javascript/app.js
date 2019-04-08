43caa3b5360981ebd9da2f8731ec235e2fa054e7
Odysseus <arwaterman@gmail.com>
2019-04-07 22:30:26 -0700

    clicking on map console logs city, state, and zip code of the position clicked

--- public/assets/javascript/app.js
+++ public/assets/javascript/app.js
@@ -10,43 +10,36 @@ function initMap() {
     
   });
 
+  const geocoder = new google.maps.Geocoder;
+
+  map.data.addListener("click", function(event) {
+
+    const lat = event.latLng.lat();
+    const lng = event.latLng.lng();
+
+    const latlng = `${lat},${lng}`
+
+    geocodeLatLng(geocoder, latlng);
+  });
+
   map.data.loadGeoJson('https://gist.githubusercontent.com/JessieSidWho/390d018648b7c4a7cbe88deb813db0a7/raw/45879ff7bf33b9364e0cb8bfe3cddea7e53091e5/sfbay.geojson');
-  
-  var geocoder = new google.maps.Geocoder;
-  // var infowindow = new google.maps.InfoWindow;
-
-  const results = geocodeLatLng(geocoder);
-
-  // console.log(results);
-  // document.getElementById('submit').addEventListener('click', function() {
-  //   geocodeLatLng(geocoder, map, infowindow);
-  // });
 }
 
-function geocodeLatLng(geocoder) {
-  // var input = document.getElementById('latlng').value;
-  const input = `37.8716,-122.2727`; // Berkeley, CA
-  var latlngStr = input.split(',', 2);
-  var latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
+function geocodeLatLng(geocoder, input) {
+  const latlngStr = input.split(',', 2);
+  const latlng = {lat: parseFloat(latlngStr[0]), lng: parseFloat(latlngStr[1])};
 
   geocoder.geocode({'location': latlng}, function(results, status) {
     if (status === 'OK') {
       if (results[0]) {
+        const address = results[0].formatted_address.split(",");
+        const stateNzip = address[2].toLowerCase().trim().split(" ");
+        const city = address[1].toLowerCase().trim();
+        const state = stateNzip[0];
+        const zip = stateNzip[1];
 
-        console.log(results);
+        console.log(city, state, zip);
 
-        console.log(results[0].address_components[2].long_name); // city
-        console.log(results[0].address_components[4].long_name); // state
-        console.log(results[0].address_components[6].long_name); // zip code
-
-        // return results;
-        // map.setZoom(11);
-        // var marker = new google.maps.Marker({
-        //   position: latlng,
-        //   map: map
-        // });
-        // infowindow.setContent(results[0].formatted_address);
-        // infowindow.open(map, marker);
       } else {
         window.alert('No results found');
       }
@@ -55,3 +48,4 @@ function geocodeLatLng(geocoder) {
     }
   });
 }
+
