3e4ddc7158eaf799d6f723a7e127ee99b962e7ee
Jessie Sidhu <jsidhu22@gmail.com>
2019-04-11 14:49:45 -0700

    add jawsdb connection

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,19 @@ const keys = require(`./keys`);
 
 const mysql = require("mysql");
 
-const connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: keys.sql.pass,
-  database: "flipsearch_db"
-});
+let connection;
+
+if (process.env.JAWSDB_URL)
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: keys.sql.pass,
+    database: "flipsearch_db"
+  });
+}
 
 connection.connect( err => {
   if (err) {
