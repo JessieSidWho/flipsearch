d844c0a32c5218726137e7fcbd66061e94cb49ce
Jessie Sidhu <jsidhu22@gmail.com>
2019-04-14 12:00:29 -0700

    login with Enter

--- public/assets/javascript/login.js
+++ public/assets/javascript/login.js
@@ -1,34 +1,71 @@
 $(`#loading`).hide();
 
+
 $(`#submit`).on(`click`, event => {
     event.preventDefault();
 
     const username = $("#username").val().trim();
     const password = $("#password").val().trim();
 
-    if(username !== "catman" || password !== "test"){
+    if (username !== "catman" || password !== "test") {
         $("#username").val("");
         $("#password").val("");
         Swal.fire({
             type: 'error',
             title: 'Oops...',
             text: 'Invalid Username or Password!',
-         })
+        })
     }
 
     $.ajax({
         url: '/api/login',
         method: 'POST',
         contentType: 'application/json',
-        data: JSON.stringify({username, password})
+        data: JSON.stringify({ username, password })
     }).then(res => {
         $(`#login`).hide();
         $(`#loading`).show();
         $(`body`).attr("style", "background-color: #5D4674 !important;")
         $(`html`).attr("style", "background-color: #5D4674 !important;")
-        
-        setTimeout( function() {
-            window.location.href="/main";
+
+        setTimeout(function () {
+            window.location.href = "/main";
         }, 3000);
     });
 });
+
+$(document).keyup(function(e) {
+    // console.log(e);
+    if (e.keyCode === 13) {
+        event.preventDefault();
+
+        const username = $("#username").val().trim();
+        const password = $("#password").val().trim();
+
+        if (username !== "catman" || password !== "test") {
+            $("#username").val("");
+            $("#password").val("");
+            Swal.fire({
+                type: 'error',
+                title: 'Oops...',
+                text: 'Invalid Username or Password!',
+            })
+        }
+
+        $.ajax({
+            url: '/api/login',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({ username, password })
+        }).then(res => {
+            $(`#login`).hide();
+            $(`#loading`).show();
+            $(`body`).attr("style", "background-color: #5D4674 !important;")
+            $(`html`).attr("style", "background-color: #5D4674 !important;")
+
+            setTimeout(function () {
+                window.location.href = "/main";
+            }, 3000);
+        });
+    }
+})
